41e111f8f9294cf275390e4879eacf92421d9f04
shaoeg <shaoeg@shjysoft.com>
2022-11-29 11:07:06 +0800

    change

---
+++ src/views/components/tabs.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="components-tabs">
+        <div class="ap-box">
+            <el-tabs v-model="state.activeName" class="demo-tabs" @tab-change="tabChange">
+                <el-tab-pane :label="item.label" :name="item.name" v-for="item in props.data"> </el-tab-pane>
+            </el-tabs>
+        </div>
+    </div>
+</template>
+<script setup>
+import { reactive, defineProps, defineEmits, onBeforeMount, onMounted } from "vue"
+const props = defineProps({
+    //标识
+    refs: {
+        type: String,
+        default: "",
+    },
+    // 处理类型
+    type: {
+        type: String,
+        default: "0",
+    },
+    // 布局
+    data: {
+        type: Array,
+        default: [],
+    },
+    //选中名字
+    activeName: {
+        type: String,
+        default: "",
+    },
+})
+const emit = defineEmits(["tab-change", "getActiveName"]);
+const state = reactive({
+    activeName: "",
+});
+console.log(props.data);
+function tabChange(pane, ev) {
+    // console.log(pane, ev);
+    emit("tab-change", state.activeName);
+}
+function getActiveName() {
+    emit("getActiveName", state.activeName);
+}
+function initActiveName() {
+    state.activeName = props.activeName
+}
+onBeforeMount(() => {
+    // console.log(`the component is now onBeforeMount.`)
+    initActiveName();
+})
+onMounted(() => {
+    // console.log(`the component is now mounted.`)
+})
+</script>
+<style lang='scss' scoped>
+.components-tabs {
+    margin: 0%;
+
+    :deep(.ap-box) {
+        .el-tabs__nav-wrap::after {
+            height: 0;
+        }
+
+        .el-tabs__item.is-active {
+            color: var(--primary-6);
+        }
+
+        .el-tabs__active-bar {
+            background-color: var(--primary-6);
+        }
+
+        .el-tabs__item:hover {
+            color: var(--primary-6);
+        }
+    }
+
+}
+</style>
