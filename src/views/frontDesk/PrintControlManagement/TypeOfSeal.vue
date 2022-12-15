be1079ed4dcfeca9aa7f42504d1c85f26e74d59f
shaoeg <shaoeg@shjysoft.com>
2022-12-15 14:08:52 +0800

    🐞 fix(增加位置替换):

--- src/views/frontDesk/PrintControlManagement/TypeOfSeal.vue
+++ src/views/frontDesk/PrintControlManagement/TypeOfSeal.vue
@@ -6,6 +6,7 @@
                 <div class="title">
                     <div>印章类型</div>
                     <div>
+                        <el-button type="primary">+ 增加</el-button>
                         <el-button>
                             <img class="button-icon" src="../../../assets/svg/gengduo-caozuo.svg" alt="" srcset="">
                             <span>更多操作</span>
@@ -29,7 +30,6 @@
 
             <template #batch>
                 <div class="batch">
-                    <el-button type="primary">+ 增加</el-button>
                 </div>
             </template>
 
