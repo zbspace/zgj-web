8d8bbc0698045b5aab71281248d95a5ef598077a
zb <zhoub@shjysoft.com>
2023-01-03 17:42:20 +0800

    🐞 fix: 新建用印申请

--- src/views/frontDesk/PrintControlManagement/Seal-application/Selection-form.vue
+++ src/views/frontDesk/PrintControlManagement/Seal-application/Selection-form.vue
@@ -1,111 +1,151 @@
 <!-- 用印申请 选中表单 -->
 <template>
-    <div class="Seal-application-Selection-form">
-        <componentsLayout Layout="title,custom">
-            <template #title>
-                <div class="title">
-                    <div>用印申请</div>
-                    <div>
-
-                    </div>
-                </div>
-            </template>
-            <template #custom>
-                <div class="custom">
-                    <div class="ap-cont-info">
-                        <div class="ap-cont-info-icon">
-                            <img class="ap-cont-info-icon-img" src="../../../../assets/svg/gantanhao-lan.svg" alt="">
-                        </div>
-                        <div class="ap-cont-info-desc">有已发起意外退出的用印申请【上海科创招投标建筑制材专属项目合同】</div>
-                        <div class="ap-cont-info-caozuo">继续完成用印申请</div>
-                    </div>
-                    <div class="ap-cont-title">
-                        请选择所需表单
-                    </div>
-                    <div class="ap-cont-desc">
-                        请根据以下步骤完善表单内容及确认审批流程
-                    </div>
-                    <div class="ap-cont-liuc">
-                        <div class="ap-cont-liuc-buzou">
-                            <div class="ap-cont-liuc-buzou-icon">
-                                <img class="ap-cont-liuc-buzou-icon-img"
-                                    src="../../../../assets/svg/yongyin-shenqing-kaishi.svg" alt="">
-                            </div>
-                            <div class="ap-cont-liuc-buzou-text">填写表单信息</div>
-                        </div>
-                        <div class="ap-cont-liuc-tubiao">
-                            <img class="ap-cont-liuc-tubiao-img"
-                                src="../../../../assets/svg/yongyin-shenqing-xiayibu.svg" alt="">
-                        </div>
-                        <div class="ap-cont-liuc-buzou">
-                            <div class="ap-cont-liuc-buzou-icon">
-                                <img class="ap-cont-liuc-buzou-icon-img"
-                                    src="../../../../assets/svg/yongyin-shenqing-liucheng.svg" alt="">
-                            </div>
-                            <div class="ap-cont-liuc-buzou-text">确认审批流程</div>
-                        </div>
-                        <div class="ap-cont-liuc-tubiao">
-                            <img class="ap-cont-liuc-tubiao-img"
-                                src="../../../../assets/svg/yongyin-shenqing-xiayibu.svg" alt="">
-                        </div>
-                        <div class="ap-cont-liuc-buzou">
-                            <div class="ap-cont-liuc-buzou-icon">
-                                <img class="ap-cont-liuc-buzou-icon-img"
-                                    src="../../../../assets/svg/yongyin-shenqing-wancheng.svg" alt="">
-                            </div>
-                            <div class="ap-cont-liuc-buzou-text">完成用印申请</div>
-                        </div>
-                    </div>
-                    <div class="ap-cont-liebiao">
-                        <div class="ap-cont-liebiao-list" v-for="n in 5" :key="n">
-                            <div class="ap-cont-liebiao-list-back">
-                                <img class="ap-cont-liebiao-list-back-img"
-                                    src="../../../../assets/svg/yongyin-shenqing-biaodan-back.svg" alt="">
-                            </div>
-                            <div class="ap-cont-liebiao-list-desc">
-                                上海科创招投标建筑制材专属项目合同
-                            </div>
-                            <div class="ap-cont-liebiao-list-but">
-                                <el-button type="primary" @click="clickListBut">去申请</el-button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </template>
-        </componentsLayout>
-    </div>
+  <div class="Seal-application-Selection-form">
+    <componentsLayout Layout="title,custom">
+      <template #title>
+        <div class="title">
+          <div>用印申请</div>
+          <div />
+        </div>
+      </template>
+      <template #custom>
+        <div class="custom">
+          <div class="ap-cont-info">
+            <div class="ap-cont-info-icon">
+              <img
+                class="ap-cont-info-icon-img"
+                src="../../../../assets/svg/gantanhao-lan.svg"
+                alt=""
+              >
+            </div>
+            <div class="ap-cont-info-desc">
+              有已发起意外退出的用印申请【上海科创招投标建筑制材专属项目合同】
+            </div>
+            <div class="ap-cont-info-caozuo">
+              继续完成用印申请
+            </div>
+          </div>
+          <div class="ap-cont-title">
+            请选择所需表单
+          </div>
+          <div class="ap-cont-desc">
+            请根据以下步骤完善表单内容及确认审批流程
+          </div>
+          <div class="ap-cont-liuc">
+            <div class="ap-cont-liuc-buzou">
+              <div class="ap-cont-liuc-buzou-icon">
+                <img
+                  class="ap-cont-liuc-buzou-icon-img"
+                  src="../../../../assets/svg/yongyin-shenqing-kaishi.svg"
+                  alt=""
+                >
+              </div>
+              <div class="ap-cont-liuc-buzou-text">
+                填写表单信息
+              </div>
+            </div>
+            <div class="ap-cont-liuc-tubiao">
+              <img
+                class="ap-cont-liuc-tubiao-img"
+                src="../../../../assets/svg/yongyin-shenqing-xiayibu.svg"
+                alt=""
+              >
+            </div>
+            <div class="ap-cont-liuc-buzou">
+              <div class="ap-cont-liuc-buzou-icon">
+                <img
+                  class="ap-cont-liuc-buzou-icon-img"
+                  src="../../../../assets/svg/yongyin-shenqing-liucheng.svg"
+                  alt=""
+                >
+              </div>
+              <div class="ap-cont-liuc-buzou-text">
+                确认审批流程
+              </div>
+            </div>
+            <div class="ap-cont-liuc-tubiao">
+              <img
+                class="ap-cont-liuc-tubiao-img"
+                src="../../../../assets/svg/yongyin-shenqing-xiayibu.svg"
+                alt=""
+              >
+            </div>
+            <div class="ap-cont-liuc-buzou">
+              <div class="ap-cont-liuc-buzou-icon">
+                <img
+                  class="ap-cont-liuc-buzou-icon-img"
+                  src="../../../../assets/svg/yongyin-shenqing-wancheng.svg"
+                  alt=""
+                >
+              </div>
+              <div class="ap-cont-liuc-buzou-text">
+                完成用印申请
+              </div>
+            </div>
+          </div>
+          <div class="ap-cont-liebiao">
+            <div
+              class="ap-cont-liebiao-list"
+              v-for="n in 5"
+              :key="n"
+            >
+              <div class="ap-cont-liebiao-list-back">
+                <img
+                  class="ap-cont-liebiao-list-back-img"
+                  src="../../../../assets/svg/yongyin-shenqing-biaodan-back.svg"
+                  alt=""
+                >
+              </div>
+              <div class="ap-cont-liebiao-list-desc">
+                上海科创招投标建筑制材专属项目合同
+              </div>
+              <div class="ap-cont-liebiao-list-but">
+                <el-button
+                  type="primary"
+                  @click="clickListBut(n)"
+                >
+                  去申请
+                </el-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </componentsLayout>
+  </div>
 </template>
 <script setup>
-import { reactive, defineProps, defineEmits, onBeforeMount, onMounted, inject } from "vue"
-import { useRouter } from 'vue-router';
-import componentsLayout from "../../../components/Layout.vue"
+import { reactive, defineProps, defineEmits, onBeforeMount, onMounted, inject } from 'vue'
+import { useRouter } from 'vue-router'
+import componentsLayout from '../../../components/Layout.vue'
 const props = defineProps({
-    // 处理类型
-    type: {
-        type: String,
-        default: "0",
-    },
+  // 处理类型
+  type: {
+    type: String,
+    default: '0'
+  }
 })
-const router = useRouter();
-const commonFun = inject("commonFun");
-const emit = defineEmits([]);
+const router = useRouter()
+const commonFun = inject('commonFun')
+const emit = defineEmits([])
 const state = reactive({
 
-});
+})
 
-//点击列表按钮
-function clickListBut() {
-    commonFun.routerPage(router, {
-        path: "/frontDesk/PrintControlManagement/Seal-application/fill-form"
-    })
+// 点击列表按钮
+function clickListBut (n) {
+  commonFun.routerPage(router, {
+    name: 'Seal-application-fill-form',
+    params: { id: n }
+  })
 }
 
 onBeforeMount(() => {
-    // console.log(`the component is now onBeforeMount.`)
+  // console.log(`the component is now onBeforeMount.`)
 
 })
 onMounted(() => {
-    // console.log(`the component is now mounted.`)
+  // console.log(`the component is now mounted.`)
 })
 </script>
 <style lang='scss' scoped>
