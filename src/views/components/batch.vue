f0327cb79e27cb636747342d765746ff43b126f7
shaoeg <shaoeg@shjysoft.com>
2023-01-05 15:10:32 +0800

    格式化

--- src/views/components/batch.vue
+++ src/views/components/batch.vue
@@ -1,102 +1,136 @@
 <template>
-    <div class="components-batch">
-        <div class="ap-box">
-            <slot></slot>
-            <div class="ap-box-right">
-                <img class="ap-box-right-icon" src="@/assets/svg/biaoge-piliang-shaxin.svg" alt="">
-                <img class="ap-box-right-icon" src="@/assets/svg/biaoge-piliang-daochu.svg" alt="">
-                <img class="ap-box-right-icon" src="@/assets/svg/biaoge-piliang-dayin.svg" alt="">
-                <img class="ap-box-right-icon" src="@/assets/svg/biaoge-piliang-quanping.svg" alt="">
-                <img class="ap-box-right-icon margin-0" src="@/assets/svg/biaoge-piliang-shezhi.svg" alt="">
-            </div>
-        </div>
+  <div class="components-batch">
+    <div class="ap-box">
+      <slot></slot>
+      <div class="ap-box-right">
+        <img
+          class="ap-box-right-icon"
+          src="@/assets/svg/biaoge-piliang-shaxin.svg"
+          alt=""
+        />
+        <img
+          class="ap-box-right-icon"
+          src="@/assets/svg/biaoge-piliang-daochu.svg"
+          alt=""
+        />
+        <img
+          class="ap-box-right-icon"
+          src="@/assets/svg/biaoge-piliang-dayin.svg"
+          alt=""
+        />
+        <img
+          class="ap-box-right-icon"
+          src="@/assets/svg/biaoge-piliang-quanping.svg"
+          alt=""
+        />
+        <img
+          class="ap-box-right-icon margin-0"
+          src="@/assets/svg/biaoge-piliang-shezhi.svg"
+          alt=""
+        />
+      </div>
     </div>
+  </div>
 </template>
 <script setup>
-import { reactive, defineProps, defineEmits, onBeforeMount, onMounted, watch } from "vue"
-const props = defineProps({
-    //标识
+  import {
+    reactive,
+    defineProps,
+    defineEmits,
+    onBeforeMount,
+    onMounted,
+    watch
+  } from 'vue'
+  const props = defineProps({
+    // 标识
     refs: {
-        type: String,
-        default: "",
+      type: String,
+      default: ''
     },
     // 处理类型
     type: {
-        type: String,
-        default: "0",
+      type: String,
+      default: '0'
     },
-    //取消底部的线
+    // 取消底部的线
     cancelBottomLine: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false
     },
     // 布局
     data: {
-        type: Array,
-        default: [],
+      type: Array,
+      default: () => {
+        return []
+      }
     },
-    //选中名字
+    // 选中名字
     activeName: {
-        type: String,
-        default: "",
-    },
-})
-const emit = defineEmits(["tab-change", "getActiveName"]);
-const state = reactive({
-    activeName: "",
+      type: String,
+      default: ''
+    }
+  })
+  const emit = defineEmits(['tab-change', 'getActiveName'])
+  const state = reactive({
+    activeName: '',
     data: [],
-    cancelBottomLine: false,
-});
-// console.log(props.data);
-function tabChange(pane, ev) {
+    cancelBottomLine: false
+  })
+  // console.log(props.data);
+  function tabChange(pane, ev) {
     // console.log(pane, ev);
-    emit("tab-change", state.activeName);
-}
-function getActiveName() {
-    emit("getActiveName", state.activeName);
-}
-//初始化数据
-function initData() {
-
-}
-watch(props, (newValue, oldValue) => {
+    emit('tab-change', state.activeName)
+  }
+  function getActiveName() {
+    emit('getActiveName', state.activeName)
+  }
+  // 初始化数据
+  function initData() {}
+  watch(props, (newValue, oldValue) => {
     // console.log(newValue, oldValue);
-    //初始化数据
+    // 初始化数据
     initData()
-})
-onBeforeMount(() => {
+  })
+  onBeforeMount(() => {
     // console.log(`the component is now onBeforeMount.`)
-    //初始化数据
+    // 初始化数据
     initData()
-})
-onMounted(() => {
+
+    // 暂时不用
+    tabChange()
+    // 暂时不用
+    getActiveName()
+  })
+  onMounted(() => {
     // console.log(`the component is now mounted.`)
-})
+  })
 </script>
-<style lang='scss' scoped>
-.components-batch {
+<style lang="scss" scoped>
+  .components-batch {
     margin: 0%;
     width: 100%;
     .ap-box {
-        width: 100%;
-        display: flex;
-        flex-flow: wrap;
-        justify-content: flex-start;
-        align-items: flex-end;
-        align-content: flex-end;
+      width: 100%;
+      display: flex;
+      flex-flow: wrap;
+      justify-content: flex-start;
+      align-items: flex-end;
+      align-content: flex-end;
 
-        .ap-box-right {
-            margin-left: auto;
+      .ap-box-right {
+        margin-left: auto;
 
-            .ap-box-right-icon {
-                margin-right: 0.5rem;
-            }
+        .ap-box-right-icon {
+          margin-right: 0.5rem;
         }
-
+        .ap-box-right-icon:hover {
+          filter: drop-shadow(var(--primary-6) 80px 0);
+        }
+      }
     }
 
     .margin-0 {
-        margin: 0% !important;
+      margin: 0% !important;
     }
-}
+  }
 </style>
