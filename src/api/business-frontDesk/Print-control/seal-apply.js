abc18597374d29e8e81440ec382ced341f3d4d01
shaoeg <shaoeg@shjysoft.com>
2023-02-21 09:35:35 +0800

    ✨ feat:添加api

---
+++ src/api/business-frontDesk/Print-control/seal-apply.js
@@ -0,0 +1,20 @@
+// api 全局配置
+import request from '@/utils/request'
+// 用印申请
+export class sealApply {
+  /**
+   * 流程列表
+   */
+
+  static add(params) {
+    const url = '/sealApply/add'
+    return request({
+      // 请求方式
+      method: 'POST',
+      // 请求的地址
+      url: url,
+      // 请求 中的查询参数
+      data: params
+    })
+  }
+}
