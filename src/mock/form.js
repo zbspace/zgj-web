e4f54d371ca6c1ab84d06a8f0f317980cedcd1fe
shaoeg <shaoeg@shjysoft.com>
2023-02-22 13:48:08 +0800

    mock修改

--- src/mock/form.js
+++ src/mock/form.js
@@ -13,8 +13,49 @@ const formPage = {
     }
   ]
 }
-
+const listApplyTypeTree = {
+  code: 200,
+  msg: null,
+  data: [
+    {
+      applyTypeId: '1',
+      applyTypeName: '用印申请',
+      parent_id: ''
+    },
+    {
+      applyTypeId: '1-1',
+      applyTypeName: '用印申请',
+      parent_id: '1'
+    },
+    {
+      applyTypeId: '1-2',
+      applyTypeName: '转办申请',
+      parent_id: '1'
+    },
+    {
+      applyTypeId: '1-3',
+      applyTypeName: '重置用印申请',
+      parent_id: '1'
+    },
+    {
+      applyTypeId: '2',
+      applyTypeName: '印章申请',
+      parent_id: ''
+    },
+    {
+      applyTypeId: '2-1',
+      applyTypeName: '刻章申请',
+      parent_id: '2'
+    },
+    {
+      applyTypeId: '2-2',
+      applyTypeName: '停用申请',
+      parent_id: '2'
+    }
+  ]
+}
 export default {
   'get|/form/page': formPage,
-  '/form/list': formPage
+  '/form/list': formPage,
+  'get|/form/listApplyTypeTree': listApplyTypeTree
 }
