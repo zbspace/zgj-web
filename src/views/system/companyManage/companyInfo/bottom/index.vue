6fd05dbe642a8fc7353f4f199fdacac21c5468c8
jayce.zhang <tzzhangjun@qq.com>
2023-03-10 12:10:58 +0800

    # 企业信息修改

--- src/views/system/companyManage/companyInfo/bottom/index.vue
+++ src/views/system/companyManage/companyInfo/bottom/index.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="comp-info-bottom">
-    <JyLabel
-      label="系统页面设置"
-      :btn="edit ? '保存' : '修改'"
-      :btn1="edit ? '取消' : ''"
-      @on-click="onClick"
-      @on-click-cancel="onClickCancel"
-    />
+    <JyLabel label="系统页面设置" />
     <div class="switch">
       <span>页面水印</span>
       <el-switch v-model="value" size="large" inactive-text="" />
     </div>
     <div class="upload">
-      <img :src="shapeIcon" alt="" />
-      <p>上传首页LOGO</p>
+      <el-upload
+        drag
+        action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+      >
+        <div class="uploadTip">
+          <img class="uploadIcon" :src="shapeIcon" />
+          <p>上传首页LOGO</p>
+        </div>
+        <!-- <el-image class="logoImage" :src="shapeIcon" fit="contain"></el-image> -->
+      </el-upload>
     </div>
     <div class="upload">
-      <img :src="shapeIcon" alt="" />
-      <p>上传登录页LOGO</p>
+      <el-upload
+        drag
+        action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+      >
+        <div class="uploadTip">
+          <img class="uploadIcon" :src="shapeIcon" />
+          <p>上传登录页LOGO</p>
+        </div>
+        <!-- <el-image class="logoImage" :src="shapeIcon" fit="contain"></el-image> -->
+      </el-upload>
     </div>
   </div>
 </template>
@@ -26,14 +36,6 @@
   import { ref } from 'vue'
   import shapeIcon from '@/assets/svg/system/comp-info/shape.svg'
   const value = ref(false)
-  const edit = ref(false)
-
-  const onClick = () => {
-    edit.value = !edit.value
-  }
-  const onClickCancel = () => {
-    edit.value = false
-  }
 </script>
 
 <style lang="scss" scoped>
@@ -41,6 +43,8 @@
     background-color: #fff;
     padding: 24px;
     .switch {
+      margin-bottom: 10px;
+
       span {
         font-weight: 400;
         font-size: 14px;
@@ -51,24 +55,43 @@
     }
     .upload {
       width: 362px;
-      height: 232px;
-      border: 2px dashed #e9ebec;
-      border-radius: 6px;
-      text-align: center;
-      padding-top: 66px;
-      cursor: pointer;
+      // height: 200px;
+      // border: 2px dashed #e9ebec;
+      // border-radius: 6px;
+      // text-align: center;
+      // padding-top: 66px;
+      // cursor: pointer;
       display: inline-block;
       margin-right: 14px;
-      img {
-        margin-bottom: 32px;
+
+      .uploadTip {
+        height: 200px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+      .uploadIcon {
+        width: 60px;
+        margin-bottom: 10px;
       }
       p {
         font-weight: 400;
         font-size: 14px;
-        line-height: 22px;
-        text-align: center;
         color: rgba(0, 0, 0, 0.65);
       }
+    }
+
+    .logoImage {
+      width: 100%;
+      height: 200px;
+    }
+  }
+</style>
+<style lang="scss">
+  .upload {
+    .el-upload-dragger {
+      padding: 10px;
     }
   }
 </style>
