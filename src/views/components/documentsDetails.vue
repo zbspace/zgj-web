df91aba06991c4131d24fa7d0970a854bae380ec
jayce.zhang <tzzhangjun@qq.com>
2023-03-31 10:58:48 +0800

    # 图标地址报错问题修复

--- src/views/components/documentsDetails.vue
+++ src/views/components/documentsDetails.vue
@@ -966,7 +966,7 @@
   import liuchengTongyiSvg from '@/assets/svg/liucheng-tongyi.svg'
   import liuchengJujueSvg from '@/assets/svg/liucheng-jujue.svg'
   import liuchengWanchengSvg from '@/assets/svg/liucheng-wancheng.svg'
-  import yuanHuiSvg from '@/assets/svg/yuan-hui.svg'
+  import yuanHuiSvg from '@/assets/svg/common/yuan-hui.svg'
   import UploadBackgroundSvg from '@/assets/svg/Upload__background.svg'
   import gaizhangIcon from '@/assets/svg/gaizhang-icon.svg'
   import renlianTupian from '@/assets/svg/renlian-tupian.svg'
